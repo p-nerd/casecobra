5d666c8704f5dc07fc941837e1ad9e8ffb7db417
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2024-05-14 00:57:20 +0600

    update

--- resources/js/Pages/Auth/VerifyEmail.tsx
+++ resources/js/Pages/Auth/VerifyEmail.tsx
@@ -1,15 +1,13 @@
 import { Button } from "@/Components/ui/button";
+import { Form } from "@/Components/ui2/form";
 import { Link, useForm } from "@inertiajs/react";
-import { FormEventHandler } from "react";
 
 import GuestLayout from "@/Layouts/GuestLayout";
 
-export default function VerifyEmail({ status }: { status?: string }) {
+const VerifyEmail = (props: { status?: string }) => {
     const { post, processing } = useForm({});
 
-    const submit: FormEventHandler = e => {
-        e.preventDefault();
-
+    const submit = () => {
         post(route("verification.send"));
     };
 
@@ -20,18 +18,15 @@ export default function VerifyEmail({ status }: { status?: string }) {
                 by clicking on the link we just emailed to you? If you didn't receive the email, we
                 will gladly send you another.
             </div>
-
-            {status === "verification-link-sent" && (
+            {props.status === "verification-link-sent" && (
                 <div className="mb-4 text-sm font-medium text-green-600">
                     A new verification link has been sent to the email address you provided during
                     registration.
                 </div>
             )}
-
-            <form onSubmit={submit}>
+            <Form onSubmit={submit}>
                 <div className="mt-4 flex items-center justify-between">
                     <Button disabled={processing}>Resend Verification Email</Button>
-
                     <Link
                         href={route("logout")}
                         method="post"
@@ -41,7 +36,9 @@ export default function VerifyEmail({ status }: { status?: string }) {
                         Log Out
                     </Link>
                 </div>
-            </form>
+            </Form>
         </GuestLayout>
     );
-}
+};
+
+export default VerifyEmail;
