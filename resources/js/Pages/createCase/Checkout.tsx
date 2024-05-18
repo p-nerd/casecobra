ff6ddc6baec4774f91dc1860d22139c225a4094a
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2024-05-18 23:16:28 +0600

    checkout page

--- resources/js/Pages/createCase/Checkout.tsx
+++ resources/js/Pages/createCase/Checkout.tsx
@@ -5,6 +5,8 @@ import { Container } from "@/components/ui2/misc";
 import Layout from "@/layouts/Layout";
 
 const Checkout = (props: TProps<{ caseDesignId: TID }>) => {
+    console.log(props);
+
     return (
         <Layout title="checkout">
             <Container>Hello</Container>
