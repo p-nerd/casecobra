442d819b5df97d9c920c68035f536eeedeab6ab8
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2024-05-26 13:48:43 +0600

    overview page

--- resources/js/Pages/dashboard/Overview.tsx
+++ resources/js/Pages/dashboard/Overview.tsx
@@ -1,17 +1,69 @@
-import { Container, Header } from "@/components/ui2/misc";
+import { formatPrice } from "@/lib/utils";
 
+import { Progress } from "@/components/ui/progress";
+import {
+    Card,
+    CardTitle,
+    CardFooter,
+    CardHeader,
+    CardContent,
+    CardDescription,
+} from "@/components/ui/card";
+import { Container, Header } from "@/components/ui2/misc";
 import SiteLayout from "@/layouts/SiteLayout";
 
-const Overview = () => {
+const LastXOrders = () => {
     return (
-        <SiteLayout title="Overview of Sells">
-            <Container className="py-12">
-                <div className="mx-auto space-y-6">
-                    <Header>Overview</Header>
-                    <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">You're logged in!</div>
-                    </div>
+        <div>
+            <Header>Last x orders</Header>
+        </div>
+    );
+};
+
+const Overview = () => {
+    const lastWeekSum = 120;
+    const lastMonthSum = 2000;
+    const WEEKLY_GOAL = 500;
+    const MONTHLY_GOAL = 2500;
+
+    return (
+        <SiteLayout title="Overview">
+            <Container className="flex flex-col gap-4 py-5">
+                <div className="grid gap-4 lg:grid-cols-2">
+                    <Card>
+                        <CardHeader className="pb-2">
+                            <CardDescription>Last Week</CardDescription>
+                            <CardTitle className="text-4xl">
+                                {formatPrice(lastWeekSum ?? 0)}
+                            </CardTitle>
+                        </CardHeader>
+                        <CardContent>
+                            <div className="text-sm text-muted-foreground">
+                                of {formatPrice(WEEKLY_GOAL)} goal
+                            </div>
+                        </CardContent>
+                        <CardFooter>
+                            <Progress value={((lastWeekSum ?? 0) * 100) / WEEKLY_GOAL} />
+                        </CardFooter>
+                    </Card>
+                    <Card>
+                        <CardHeader className="pb-2">
+                            <CardDescription>Last Month</CardDescription>
+                            <CardTitle className="text-4xl">
+                                {formatPrice(lastMonthSum ?? 0)}
+                            </CardTitle>
+                        </CardHeader>
+                        <CardContent>
+                            <div className="text-sm text-muted-foreground">
+                                of {formatPrice(MONTHLY_GOAL)} goal
+                            </div>
+                        </CardContent>
+                        <CardFooter>
+                            <Progress value={((lastMonthSum ?? 0) * 100) / MONTHLY_GOAL} />
+                        </CardFooter>
+                    </Card>
                 </div>
+                <LastXOrders />
             </Container>
         </SiteLayout>
     );
